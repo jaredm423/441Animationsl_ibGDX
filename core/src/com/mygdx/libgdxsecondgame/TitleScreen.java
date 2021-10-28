7838f152af1df3c16e4d14fad288f5d9da66714a
Jared Montgomery <jmontgo5@binghamton.edu>
2021-10-28 19:15:12 -0400

    fixed game crashing, it is ugly tho

--- core/src/com/mygdx/libgdxsecondgame/TitleScreen.java
+++ core/src/com/mygdx/libgdxsecondgame/TitleScreen.java
@@ -5,14 +5,41 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class TitleScreen extends ScreenAdapter {
 
     SecondGdxGame game;
+    //screen
+    private Camera camera;
+    private Viewport viewport;
+
+    //graphics
+    private SpriteBatch batch;
+    private Texture background;
+
+    //timing
+    private int backgroundOffset;
+
+    //world parameters
+    private final int WORLD_WIDTH = 72;
+    private final int WORLD_HEIGHT = 128;
 
     public TitleScreen(SecondGdxGame game) {
         this.game = game;
+        camera = new OrthographicCamera();
+        viewport = new StretchViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+
+        background = new Texture("RainyBackground.png");
+        backgroundOffset = 0;
+
+        batch = new SpriteBatch();
     }
 
     @Override
@@ -26,14 +53,29 @@ public class TitleScreen extends ScreenAdapter {
         });
     }
     @Override
-    public void render(float delta){
+    public void render(float deltaT){
+        //scrolling background
+        backgroundOffset++;
+        if(backgroundOffset > WORLD_HEIGHT)
+            backgroundOffset = 0;
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         game.batch.begin();
+        //background
+        game.batch.draw(background, 0,-backgroundOffset, WORLD_WIDTH, WORLD_HEIGHT);
+        game.batch.draw(background, 0, -backgroundOffset + WORLD_HEIGHT,WORLD_WIDTH, WORLD_HEIGHT);
+        //text
         game.font.getData().setScale(5);
         game.font.draw(game.batch, "Title Screen!", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
         game.font.draw(game.batch, "Tap if you dare...", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .25f);
         game.batch.end();
+    }
+    @Override
+    public void resize(int w, int h){
+        viewport.update(w, h, true);
+        batch.setProjectionMatrix(camera.combined);
     }
     @Override
     public void hide(){
